bef0e947fd3574c841954fdd2a5ba450ade703ad
Maykom_Douglas <maykomdouglas.1995@gmail.com>
2022-07-04 19:48:38 -0300

    Resolução do requisito 4

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class Header extends React.Component {
   render() {
     const { userNome } = this.props;
     return (
       <header data-testid="header-component">
-        {' '}
         <p data-testid="header-user-name">
           {userNome}
         </p>
-        {' '}
+        <nav>
+          <Link to="/search" data-testid="link-to-search"> Pesquisar </Link>
+          <Link to="/favorites" data-testid="link-to-favorites"> músicas favoritas </Link>
+          <Link to="/profile" data-testid="link-to-profile"> perfil </Link>
+        </nav>
       </header>);
   }
 }
