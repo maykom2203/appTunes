06b5343ef6d868d5281feca7cb670a01e45f9093
Maykom_Douglas <maykomdouglas.1995@gmail.com>
2022-07-07 09:29:02 -0300

    Resolução do requisito 8 e 10, metade do 9 e do 11

--- src/pages/Album.js
+++ src/pages/Album.js
@@ -2,31 +2,57 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import getMusics from '../services/musicsAPI';
+import Carregando from '../components/Carregando';
+import { addSong, getFavoriteSongs, removeSong } from '../services/favoriteSongsAPI';
 
 class Album extends React.Component {
   constructor() {
     super();
     this.state = {
+      favorites: [],
       banda: [],
       album: '',
       artista: '',
+      carregando: false,
+
     };
   }
 
   componentDidMount() {
     const { match: { params: { id } } } = this.props;
     this.Musics(id);
+    this.cardMusic();
   }
+
+  componentDidUpdate() {
+    this.cardMusic();
+  }
+
+  cardMusic = async () => {
+    const saveFavorite = await getFavoriteSongs();
+    this.setState({ favorites: saveFavorite });
+  };
 
   Musics = async (id) => {
     const musicas = await getMusics(id);
-    this.setState({ banda: musicas,
+    this.setState({
+      banda: musicas,
       album: musicas[0].collectionName,
-      artista: musicas[0].artistName });
+      artista: musicas[0].artistName,
+    });
+  }
+
+  favoritChek = async (artist, event) => {
+    this.setState({ carregando: true });
+    await addSong(artist);
+    this.setState({ carregando: false });
+    if (event.target.checked) {
+      removeSong(artist);
+    }
   }
 
   render() {
-    const { banda, album, artista } = this.state;
+    const { banda, album, artista, carregando, favorites } = this.state;
     return (
       <div data-testid="page-album">
         <Header />
@@ -36,18 +62,32 @@ class Album extends React.Component {
         <div data-testid="artist-name">
           {artista}
         </div>
+        {carregando ? <Carregando />
 
-        {banda.slice(1).map((artist, i) => (
-          <>
-            <div key={ i } />
-            <p>{artist.trackName}</p>
-            <audio data-testid="audio-component" src={ artist.previewUrl } controls>
-              <track kind="captions" />
-              <code>audio</code>
-            </audio>
+          : banda.slice(1).map((artist, i) => (
+            <>
+              <div key={ i } />
+              <p>{artist.trackName}</p>
+              <audio data-testid="audio-component" src={ artist.previewUrl } controls>
+                <track kind="captions" />
+                <code>audio</code>
+              </audio>
+              <form>
+                <label htmlFor="checkbox-music">
+                  Favorita
+                  <input
+                    type="checkbox"
+                    data-testid={ `checkbox-music-${artist.trackId}` }
+                    onChange={ (event) => this.favoritChek(artist, event) }
+                    checked={ favorites.some((fav) => (
+                      fav.trackId === artist.trackId)) }
 
-          </>
-        ))}
+                  />
+                </label>
+              </form>
+            </>
+          ))}
+
       </div>
 
     );
@@ -55,11 +95,19 @@ class Album extends React.Component {
 }
 
 Album.propTypes = {
-  match: PropTypes.objectOf(PropTypes.string),
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }),
 
 };
 
 Album.defaultProps = {
-  match: 'string',
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }),
 };
 export default Album;
